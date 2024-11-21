108e8a7e0957cb2d7a5b27b014ffafd4c2f3811e
pavithran.muthusamy <pavithran.muthusamy@intellectdesign.com>
2024-11-21 16:49:52 +0530

    ds

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,21 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'es',
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash][extname]'
+      }
     }
   }
 })
