ac1de63715a488a32a002812c230edc21b870410
pavithran.muthusamy <pavithran.muthusamy@intellectdesign.com>
2024-11-21 16:25:49 +0530

    s

---
+++ src/components/Example.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Example',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      message: 'This is a TypeScript component'
+    }
+  }
+})
+</script> 
---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Movie {
+  id: number
+  title: string
+  description: string
+  rating: number
+  releaseDate: Date
+}
+
+export interface User {
+  id: number
+  username: string
+  email: string
+} 
